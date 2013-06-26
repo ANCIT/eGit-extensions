6e77528c961a75fb4d2564d61a0854e7a8016ed0
Annamalai C <annamalai.chockalingam@gmail.com>
2013-06-26 18:10:45 +0530

    Fixed : Invalid PullRequest and Other Validations to Stabilise PullRequest., Removed unwanted Dependancies

--- org.ancit.github.utils.pr/src/org/ancit/github/utils/pr/wizards/PullRequestWizardPage.java
+++ org.ancit.github.utils.pr/src/org/ancit/github/utils/pr/wizards/PullRequestWizardPage.java
@@ -2,7 +2,6 @@ package org.ancit.github.utils.pr.wizards;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.List;
 
 import org.ancit.github.utils.pr.dialog.AuthenticationDialog;
@@ -12,33 +11,31 @@ import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.PullRequestService;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
-import org.eclipse.equinox.security.storage.ISecurePreferences;
-import org.eclipse.equinox.security.storage.SecurePreferencesFactory;
-import org.eclipse.equinox.security.storage.StorageException;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.CredentialItem.Username;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
@@ -53,6 +50,9 @@ public class PullRequestWizardPage extends WizardPage {
 	private int TO_BRANCH = 2;
 	private String repositoryName;
 	private Browser browser;
+	private Text txtTitle;
+	private Text txtDescription;
+	private String user;
 
 	
 	/**
@@ -78,6 +78,25 @@ public class PullRequestWizardPage extends WizardPage {
 		
 		Composite container = new Composite(parent, SWT.NULL);
 		container.setLayout(new GridLayout(4, false));
+			
+			Group grpPullRequestInfo = new Group(container, SWT.NONE);
+			grpPullRequestInfo.setLayout(new GridLayout(2, false));
+			grpPullRequestInfo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 4, 1));
+			grpPullRequestInfo.setText("Pull Request Info");
+			
+			Label lblTitle = new Label(grpPullRequestInfo, SWT.NONE);
+			lblTitle.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			lblTitle.setText("Title");
+			
+			txtTitle = new Text(grpPullRequestInfo, SWT.BORDER);
+			txtTitle.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label lblDescription = new Label(grpPullRequestInfo, SWT.NONE);
+			lblDescription.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			lblDescription.setText("Description");
+			
+			txtDescription = new Text(grpPullRequestInfo, SWT.BORDER);
+			txtDescription.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
 			final Combo toBranch = new Combo(container, SWT.READ_ONLY);
 			toBranch.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
@@ -93,7 +112,11 @@ public class PullRequestWizardPage extends WizardPage {
 			
 			Button btnGeneratePullRequest = new Button(container, SWT.NONE);
 			
-			btnGeneratePullRequest.setText("Pull");
+			btnGeneratePullRequest.setText("Create");
+			new Label(container, SWT.NONE);
+			new Label(container, SWT.NONE);
+			new Label(container, SWT.NONE);
+			new Label(container, SWT.NONE);
 		
 			browser = new Browser(container, SWT.NONE);
 			browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 4, 1));
@@ -129,25 +152,20 @@ public class PullRequestWizardPage extends WizardPage {
 		btnGeneratePullRequest.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				if(toBranch.getText().length() > 0 && fromBranch.getText().length() > 0) {
+				if(toBranch.getText().length() > 0 && fromBranch.getText().length() > 0 && txtDescription.getText().length() > 0 && txtTitle.getText().length() > 0) {
 					getBranchConfiguration(toBranch, TO_BRANCH);
 					getBranchConfiguration(fromBranch,FROM_BRANCH);
-					browser.setUrl(createURL());
-					setMessage(null);
-					
+//					browser.setUrl(createURL());
+					setMessage(null);					
 					generatePullRequest();
+				} else {
+					setErrorMessage("Enter Valid Information in Title/Description.");
 				}
 			}
 		});
 		
 		setControl(container);
 	}
-
-	private String createURL() {
-		String url = baseURL + toBranchName +"..."+fromBranchName;
-		return url;
-	}
-
 
 	private void getBranchConfiguration(final Combo branch, int type) {
 		StoredConfig config = myRepository.getConfig();
@@ -183,10 +201,6 @@ public class PullRequestWizardPage extends WizardPage {
 		}
 	}
 	
-	
-	
-
-
 	protected void generatePullRequest() {
 		// TODO Auto-generated method stub
 		try {
@@ -196,8 +210,8 @@ public class PullRequestWizardPage extends WizardPage {
 			PullRequestService prService = new PullRequestService(client);
 			
 			PullRequest request = new PullRequest();
-			request.setBody("A fix");
-			request.setTitle("This is a fix");
+			request.setBody(txtDescription.getText());
+			request.setTitle(txtTitle.getText());
 			PullRequestMarker headMarker = new PullRequestMarker();
 			headMarker.setRef(fromBranchName);
 			headMarker.setLabel(fromBranchName);
@@ -221,24 +235,17 @@ public class PullRequestWizardPage extends WizardPage {
 			
 		} catch (IOException e) {
 			setErrorMessage(e.getMessage());
+			e.printStackTrace();
+			browser.setUrl("https://github.com/"+toBranchName.split(":")[0]+"/"+repositoryName+"/pulls");
 		}
 
 		
 	}
 	
-	
-	/**
-	 * Create client for url
-	 *
-	 * @param url
-	 * @return client
-	 * @throws IOException
-	 */
-	protected GitHubClient createClient() throws IOException {
-		GitHubClient client = null;
-			client = new GitHubClient();
-		return configure(client);
-	}
+	 private String createURL() {
+		  String url = baseURL + toBranchName +"..."+fromBranchName;
+		  return url;
+	 }
 	
 	/**
 	 * Configure client
@@ -251,9 +258,10 @@ public class PullRequestWizardPage extends WizardPage {
 				.getDefault().getActiveShell());
 
 		if (IDialogConstants.OK_ID == dialog.open()) {
-			String user = dialog.getUsername();
+			user = dialog.getUsername();
 			String password = dialog.getPasword();
 			client.setCredentials(user, password);
+			client.setUserAgent(user);
 		}
 		return client;
 	}
